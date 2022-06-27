7da429a1d6c8d0b6df0aba37f6682bcae2ab71a3
Urb4nOutl4w <tim.braunauer@edu.fh-joanneum.at>
2022-06-27 12:12:13 +0200

    first commit

---
+++ TestAppXml/Class1.cs
@@ -0,0 +1,355 @@
+﻿/* 
+ Licensed under the Apache License, Version 2.0
+
+ http://www.apache.org/licenses/LICENSE-2.0
+ */
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace Xml2CSharp
+{
+	[XmlRoot(ElementName = "participant", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class Participant
+	{
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName = "processRef")]
+		public string ProcessRef { get; set; }
+		[XmlElement(ElementName = "participantMultiplicity", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public ParticipantMultiplicity ParticipantMultiplicity { get; set; }
+	}
+
+	[XmlRoot(ElementName = "participantMultiplicity", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class ParticipantMultiplicity
+	{
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "maximum")]
+		public string Maximum { get; set; }
+		[XmlAttribute(AttributeName = "minimum")]
+		public string Minimum { get; set; }
+	}
+
+	[XmlRoot(ElementName = "messageFlow", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class MessageFlow
+	{
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName = "sourceRef")]
+		public string SourceRef { get; set; }
+		[XmlAttribute(AttributeName = "targetRef")]
+		public string TargetRef { get; set; }
+	}
+
+	[XmlRoot(ElementName = "collaboration", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class Collaboration
+	{
+		[XmlElement(ElementName = "participant", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public List<Participant> Participant { get; set; }
+		[XmlElement(ElementName = "messageFlow", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public List<MessageFlow> MessageFlow { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+	}
+
+	[XmlRoot(ElementName = "lane", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class Lane
+	{
+		[XmlElement(ElementName = "flowNodeRef", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public List<string> FlowNodeRef { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+	}
+
+	[XmlRoot(ElementName = "laneSet", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class LaneSet
+	{
+		[XmlElement(ElementName = "lane", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public List<Lane> Lane { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+	}
+
+	[XmlRoot(ElementName = "startEvent", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class StartEvent
+	{
+		[XmlElement(ElementName = "outgoing", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string Outgoing { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlElement(ElementName = "messageEventDefinition", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public MessageEventDefinition MessageEventDefinition { get; set; }
+		[XmlAttribute(AttributeName = "isInterrupting")]
+		public string IsInterrupting { get; set; }
+	}
+
+	[XmlRoot(ElementName = "task", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class Task
+	{
+		[XmlElement(ElementName = "incoming", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string Incoming { get; set; }
+		[XmlElement(ElementName = "outgoing", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string Outgoing { get; set; }
+		[XmlAttribute(AttributeName = "completionQuantity")]
+		public string CompletionQuantity { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "isForCompensation")]
+		public string IsForCompensation { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName = "startQuantity")]
+		public string StartQuantity { get; set; }
+	}
+
+	[XmlRoot(ElementName = "messageEventDefinition", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class MessageEventDefinition
+	{
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+	}
+
+	[XmlRoot(ElementName = "endEvent", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class EndEvent
+	{
+		[XmlElement(ElementName = "incoming", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string Incoming { get; set; }
+		[XmlElement(ElementName = "messageEventDefinition", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public MessageEventDefinition MessageEventDefinition { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+	}
+
+	[XmlRoot(ElementName = "sequenceFlow", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class SequenceFlow
+	{
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName = "sourceRef")]
+		public string SourceRef { get; set; }
+		[XmlAttribute(AttributeName = "targetRef")]
+		public string TargetRef { get; set; }
+	}
+
+	[XmlRoot(ElementName = "process", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class Process
+	{
+		[XmlElement(ElementName = "extensionElements", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string ExtensionElements { get; set; }
+		[XmlElement(ElementName = "laneSet", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public LaneSet LaneSet { get; set; }
+		[XmlElement(ElementName = "startEvent", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public StartEvent StartEvent { get; set; }
+		[XmlElement(ElementName = "task", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public List<Task> Task { get; set; }
+		[XmlElement(ElementName = "endEvent", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public EndEvent EndEvent { get; set; }
+		[XmlElement(ElementName = "sequenceFlow", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public List<SequenceFlow> SequenceFlow { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "isClosed")]
+		public string IsClosed { get; set; }
+		[XmlAttribute(AttributeName = "isExecutable")]
+		public string IsExecutable { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName = "processType")]
+		public string ProcessType { get; set; }
+		[XmlElement(ElementName = "sendTask", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public SendTask SendTask { get; set; }
+		[XmlElement(ElementName = "textAnnotation", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public TextAnnotation TextAnnotation { get; set; }
+	}
+
+	[XmlRoot(ElementName = "sendTask", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class SendTask
+	{
+		[XmlElement(ElementName = "incoming", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string Incoming { get; set; }
+		[XmlElement(ElementName = "outgoing", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string Outgoing { get; set; }
+		[XmlAttribute(AttributeName = "completionQuantity")]
+		public string CompletionQuantity { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "implementation")]
+		public string Implementation { get; set; }
+		[XmlAttribute(AttributeName = "isForCompensation")]
+		public string IsForCompensation { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName = "startQuantity")]
+		public string StartQuantity { get; set; }
+	}
+
+	[XmlRoot(ElementName = "textAnnotation", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class TextAnnotation
+	{
+		[XmlElement(ElementName = "text", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public string Text { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "alignment", Namespace = "http://www.signavio.com")]
+		public string Alignment { get; set; }
+	}
+
+	[XmlRoot(ElementName = "Bounds", Namespace = "http://www.omg.org/spec/DD/20100524/DC")]
+	public class Bounds
+	{
+		[XmlAttribute(AttributeName = "height")]
+		public string Height { get; set; }
+		[XmlAttribute(AttributeName = "width")]
+		public string Width { get; set; }
+		[XmlAttribute(AttributeName = "x")]
+		public string X { get; set; }
+		[XmlAttribute(AttributeName = "y")]
+		public string Y { get; set; }
+	}
+
+	[XmlRoot(ElementName = "BPMNLabel", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+	public class BPMNLabel
+	{
+		[XmlElement(ElementName = "Bounds", Namespace = "http://www.omg.org/spec/DD/20100524/DC")]
+		public Bounds Bounds { get; set; }
+		[XmlAttribute(AttributeName = "labelStyle")]
+		public string LabelStyle { get; set; }
+	}
+
+	[XmlRoot(ElementName = "BPMNShape", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+	public class BPMNShape
+	{
+		[XmlElement(ElementName = "Bounds", Namespace = "http://www.omg.org/spec/DD/20100524/DC")]
+		public Bounds Bounds { get; set; }
+		[XmlElement(ElementName = "BPMNLabel", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+		public BPMNLabel BPMNLabel { get; set; }
+		[XmlAttribute(AttributeName = "bpmnElement")]
+		public string BpmnElement { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "isHorizontal")]
+		public string IsHorizontal { get; set; }
+	}
+
+	[XmlRoot(ElementName = "waypoint", Namespace = "http://www.omg.org/spec/DD/20100524/DI")]
+	public class Waypoint
+	{
+		[XmlAttribute(AttributeName = "x")]
+		public string X { get; set; }
+		[XmlAttribute(AttributeName = "y")]
+		public string Y { get; set; }
+	}
+
+	[XmlRoot(ElementName = "BPMNEdge", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+	public class BPMNEdge
+	{
+		[XmlElement(ElementName = "waypoint", Namespace = "http://www.omg.org/spec/DD/20100524/DI")]
+		public List<Waypoint> Waypoint { get; set; }
+		[XmlAttribute(AttributeName = "bpmnElement")]
+		public string BpmnElement { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlElement(ElementName = "BPMNLabel", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+		public BPMNLabel BPMNLabel { get; set; }
+	}
+
+	[XmlRoot(ElementName = "BPMNPlane", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+	public class BPMNPlane
+	{
+		[XmlElement(ElementName = "BPMNShape", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+		public List<BPMNShape> BPMNShape { get; set; }
+		[XmlElement(ElementName = "BPMNEdge", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+		public List<BPMNEdge> BPMNEdge { get; set; }
+		[XmlAttribute(AttributeName = "bpmnElement")]
+		public string BpmnElement { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+	}
+
+	[XmlRoot(ElementName = "Font", Namespace = "http://www.omg.org/spec/DD/20100524/DC")]
+	public class Font
+	{
+		[XmlAttribute(AttributeName = "isBold")]
+		public string IsBold { get; set; }
+		[XmlAttribute(AttributeName = "isItalic")]
+		public string IsItalic { get; set; }
+		[XmlAttribute(AttributeName = "isStrikeThrough")]
+		public string IsStrikeThrough { get; set; }
+		[XmlAttribute(AttributeName = "isUnderline")]
+		public string IsUnderline { get; set; }
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName = "size")]
+		public string Size { get; set; }
+	}
+
+	[XmlRoot(ElementName = "BPMNLabelStyle", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+	public class BPMNLabelStyle
+	{
+		[XmlElement(ElementName = "Font", Namespace = "http://www.omg.org/spec/DD/20100524/DC")]
+		public Font Font { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+	}
+
+	[XmlRoot(ElementName = "BPMNDiagram", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+	public class BPMNDiagram
+	{
+		[XmlElement(ElementName = "BPMNPlane", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+		public BPMNPlane BPMNPlane { get; set; }
+		[XmlElement(ElementName = "BPMNLabelStyle", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+		public List<BPMNLabelStyle> BPMNLabelStyle { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+	}
+
+	[XmlRoot(ElementName = "definitions", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+	public class Definitions
+	{
+		[XmlElement(ElementName = "collaboration", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public Collaboration Collaboration { get; set; }
+		[XmlElement(ElementName = "process", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+		public List<Process> Process { get; set; }
+		[XmlElement(ElementName = "BPMNDiagram", Namespace = "http://www.omg.org/spec/BPMN/20100524/DI")]
+		public BPMNDiagram BPMNDiagram { get; set; }
+		[XmlAttribute(AttributeName = "xmlns")]
+		public string Xmlns { get; set; }
+		[XmlAttribute(AttributeName = "bpmndi", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Bpmndi { get; set; }
+		[XmlAttribute(AttributeName = "omgdc", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Omgdc { get; set; }
+		[XmlAttribute(AttributeName = "omgdi", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Omgdi { get; set; }
+		[XmlAttribute(AttributeName = "signavio", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Signavio { get; set; }
+		[XmlAttribute(AttributeName = "xsi", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Xsi { get; set; }
+		[XmlAttribute(AttributeName = "exporter")]
+		public string Exporter { get; set; }
+		[XmlAttribute(AttributeName = "exporterVersion")]
+		public string ExporterVersion { get; set; }
+		[XmlAttribute(AttributeName = "expressionLanguage")]
+		public string ExpressionLanguage { get; set; }
+		[XmlAttribute(AttributeName = "id")]
+		public string Id { get; set; }
+		[XmlAttribute(AttributeName = "targetNamespace")]
+		public string TargetNamespace { get; set; }
+		[XmlAttribute(AttributeName = "typeLanguage")]
+		public string TypeLanguage { get; set; }
+		[XmlAttribute(AttributeName = "schemaLocation", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+		public string SchemaLocation { get; set; }
+	}
+
+}
---
+++ TestAppXml/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using alps.net.api;
+using alps.net.api.ALPS;
+using alps.net.api.parsing;
+using alps.net.api.StandardPASS;
+
+namespace TestAppXml
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Program test = new Program();
+            test.ReadXML();
+            test.WriteOWL();
+            
+            
+            Console.WriteLine("test");
+        }
+
+
+        public void WriteOWL()
+        {
+            // import
+            IPASSReaderWriter io = PASSReaderWriter.getInstance();
+
+            // Prepare the paths to the structure-defining owl files
+            IList<string> paths = new List<string>
+            {
+                "C:/Users/Tim/OneDrive/Documents/PASS/standard_PASS_ont_v_1.1.0.owl",
+                "C:/Users/Tim/OneDrive/Documents/PASS/abstract-layered-pass-ont.owl",
+            };
+            // Load these files once (no future calls neded)
+            io.loadOWLParsingStructure(paths);
+
+            IList<IPASSProcessModel> models = io.loadModels(new List<string> { "C:/Users/Tim/Dropbox/FH Joanneum/MAB/Visio/Drawing1.owl" });
+
+            //var test2 = models[0]"test";
+
+            // ################################################### export ####################################################################
+
+            // Create Model
+            IPASSProcessModel model = new PASSProcessModel("http://subjective-me.jimdo.com/s-bpm/processmodels/2022-05-18/tim/", "Drawing2");
+            //model.createUniqueModelComponentID();
+            //model.setModelComponentID("1234");
+
+            // Create SID
+            IModelLayer layer = new ModelLayer(model, "SID_1");
+            //layer.createUniqueModelComponentID();
+
+            // The layer is already registered in the model (because the model was passed in the constructor),
+            // but the layer is currently not the base layer
+            model.setBaseLayer(layer);
+
+            // Create Subject with Base Behavior
+
+            ISubjectBaseBehavior beTim = new SubjectBaseBehavior(layer, "Tim_Behavior");
+            IFullySpecifiedSubject tim = new FullySpecifiedSubject(layer, "Tim", null, beTim);
+
+            ISubjectBaseBehavior beLukas = new SubjectBaseBehavior(layer, "Lukas_Behavior");
+            IFullySpecifiedSubject lukas = new FullySpecifiedSubject(layer, "Lukas", null, beLukas);
+
+            // Create Message Exchange
+
+            IMessageExchangeList timToLukas = new MessageExchangeList(layer, "TimToLukas");
+            IMessageSpecification message1 = new MessageSpecification(layer, "Gemma essen");
+            IMessageExchange messageEx1 = new MessageExchange(layer, "Gemma Essen von Tim an Lukas", message1, tim, lukas);
+            timToLukas.addContainsMessageExchange(messageEx1);
+
+            IMessageExchangeList lukasToTim = new MessageExchangeList(layer, "LukasToTim");
+            IMessageSpecification message2 = new MessageSpecification(layer, "Jo");
+            IMessageExchange messageEx2 = new MessageExchange(layer, "Jo von Lukas an Tim", message2, lukas, tim);
+            lukasToTim.addContainsMessageExchange(messageEx2);
+
+            IMessageSpecification message3 = new MessageSpecification(layer, "Na");
+            IMessageExchange messageEx3 = new MessageExchange(layer, "Na von Lukas an Tim", message3, lukas, tim);
+            lukasToTim.addContainsMessageExchange(messageEx3);
+
+            // Create Behavior
+
+            IDoState doState1 = new DoState(beTim, "essen");
+            //doState1.setIsStateType(IState.StateType.InitialStateOfBehavior);
+            ISendState sendState1 = new SendState(beTim, "senden");
+            IReceiveState receiveState1 = new ReceiveState(beTim, "empfangen");
+            IDoState doState2 = new DoState(beTim, "essen2");
+            IDoState doState3 = new DoState(beTim, "essen3");
+
+            beTim.setInitialState(doState1);
+            ISet<IState> endStates = new HashSet<IState>();
+            endStates.Add(doState2);
+            endStates.Add(doState3);
+            beTim.setEndStates(endStates);
+
+
+            IDoTransition doTransition1 = new DoTransition(doState1, sendState1, "essen zu senden");
+
+            ISendTransition sendTran1 = new SendTransition(sendState1, receiveState1, "senden zu empfangen");
+            ISendTransitionCondition sendTranCon1 = new SendTransitionCondition(sendTran1, "Send Transition Condition", null, messageEx1, 0, 0, null, messageEx1.getReceiver(), messageEx1.getMessageType());
+            sendTran1.setTransitionCondition(sendTranCon1);
+            
+
+            IReceiveTransition recTran1 = new ReceiveTransition(receiveState1, doState2, "von empfangen zu essen2");
+            IReceiveTransitionCondition recTranCon1 = new ReceiveTransitionCondition(recTran1, "Receive Transition Vondition", null, messageEx2, 0, 0, null, messageEx2.getSender(), messageEx2.getMessageType());
+            recTran1.setTransitionCondition(recTranCon1);
+
+            IReceiveTransition recTran2 = new ReceiveTransition(receiveState1, doState3, "von empfangen zu essen3");
+            IReceiveTransitionCondition recTranCon2 = new ReceiveTransitionCondition(recTran2, "Receive Transition Vondition", null, messageEx3, 0, 0, null, messageEx3.getSender(), messageEx3.getMessageType());
+            recTran2.setTransitionCondition(recTranCon2);
+
+
+
+            Console.WriteLine(io.exportModel(model, "C:/Users/Tim/Dropbox/FH Joanneum/MAB/Visio/Drawing2.owl"));
+
+        }
+
+
+        public String RemoveTags(string text, string startTag, string endTag)
+        {
+            if (text.Contains(startTag))
+            {
+                // Find the position of the start and the end tag
+                int foundS1 = text.IndexOf(startTag);
+                int foundS2 = text.IndexOf(endTag);
+
+                //RemoveTags everything between start and end tag
+                text = text.Remove(foundS1, foundS2 + endTag.Length - foundS1);
+                
+                // call the function again (recursion)
+                return RemoveTags(text, startTag, endTag);
+            }
+            else
+            {
+                //end of recursion
+                return text;
+            }
+        }
+
+        
+
+        public void ReadXML()
+        {
+            /*
+            // First write something so that there is something to read ...  
+            var b = new Book { title = "Serialization Overview" };
+            var serializer = new XmlSerializer(typeof(Book));
+            var wfile = new System.IO.StreamWriter(@"C:\Users\Tim\Dropbox\FH Joanneum\MAB\Visio\test.xml");
+            serializer.Serialize(wfile, b);
+            wfile.Close();
+
+            */
+
+            string rawFile = File.ReadAllText(@"C:\Users\Tim\Dropbox\FH Joanneum\MAB\BPMN\Testprozess Lanes.bpmn");
+
+            string cleanFile = RemoveTags(rawFile, "<extensionElements>", "</extensionElements>");
+
+            XDocument xdoc = XDocument.Parse(cleanFile);
+
+            xdoc.Save(@"C:\Users\Tim\Dropbox\FH Joanneum\MAB\BPMN\Testprozess Lanes_clean.bpmn");
+
+
+            XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(Xml2CSharp.Definitions));
+            System.IO.StreamReader file = new System.IO.StreamReader(
+                @"C:\Users\Tim\Dropbox\FH Joanneum\MAB\BPMN\Testprozess Lanes_clean.bpmn");
+            Xml2CSharp.Definitions overview = (Xml2CSharp.Definitions)reader.Deserialize(file);
+            file.Close();
+
+            foreach (Xml2CSharp.Participant p in overview.Collaboration.Participant)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            //Console.WriteLine(overview);
+
+        }
+
+    }
+}
